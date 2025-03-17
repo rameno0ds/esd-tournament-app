7a6550b77904c2727447f524470b70b5684d4db3
rameno0ds <ranen.tan.2023@scis.smu.edu.sg>
2025-03-17 16:57:21 +0800

    reorg file structures, added flask-cor backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
+axios
+  .get("http://127.0.0.1:5000/")
+  .then((response) => console.log(response.data))
+  .catch((error) => console.error("Error:", error));
 
 const app = createApp(App);
 app.use(router);
